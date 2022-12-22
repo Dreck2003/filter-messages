247e2581af3a4b8fa81e6c9c6f4da76e007114b1
dreck2003 <dikson1.aranda.cabrera@gmail.com>
2022-12-22 14:09:56 -0500

    feat(frontend): add wasm rust to frontend

--- app/frontend/src/main.ts
+++ app/frontend/src/main.ts
@@ -1,5 +1,12 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
+import init from "helpers-wasm";
 
-createApp(App).mount("#app");
+init()
+	.then(() => {
+		createApp(App).mount("#app");
+	})
+	.catch((err) => {
+		console.log("error load the file:", err);
+	});
