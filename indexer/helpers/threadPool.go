a54b5989cd159e868ac3fb9123260314cebc27d8
dreck2003 <dikson1.aranda.cabrera@gmail.com>
2022-12-24 17:58:05 -0500

    feat(indexer->helpers): add two ways to work with goroutines

---
+++ indexer/helpers/threadPool.go
@@ -0,0 +1,70 @@
+package helpers
+
+import (
+	"sync"
+)
+
+type handleFunc = func(...any)
+
+type Job struct {
+	Cancel     bool
+	HandleFunc handleFunc
+	Params     []any
+}
+
+func CreateWorker(id uint, c chan Job, wg *sync.WaitGroup) {
+	go func(id uint, c chan Job, wg *sync.WaitGroup) {
+		for {
+			job := <-c
+			if job.Cancel {
+				wg.Done()
+				break
+			}
+			job.HandleFunc(job.Params...)
+		}
+	}(id, c, wg)
+}
+
+type ThreadPool struct {
+	channel chan Job
+	wg      *sync.WaitGroup
+	maxSize uint
+}
+
+func NewThreadPool(size uint) *ThreadPool {
+
+	channel := make(chan Job)
+	wg := new(sync.WaitGroup)
+
+	for i := 0; i < int(size); i++ {
+		wg.Add(1)
+		CreateWorker(uint(i), channel, wg)
+	}
+
+	return &ThreadPool{
+		channel: channel,
+		wg:      wg,
+		maxSize: size,
+	}
+}
+
+func (t *ThreadPool) Execute(f handleFunc, params ...any) {
+	t.channel <- Job{
+		Cancel:     false,
+		HandleFunc: f,
+		Params:     params,
+	}
+}
+
+func (t *ThreadPool) Wait() {
+	defer t.wg.Wait()
+	for i := 0; i < int(t.maxSize); i++ {
+		t.channel <- Job{
+			Cancel: true,
+			HandleFunc: func(p ...any) {
+			},
+			Params: []any{},
+		}
+	}
+
+}
---
+++ indexer/helpers/workerPool.go
@@ -0,0 +1,90 @@
+package helpers
+
+import (
+	"sync"
+)
+
+type handleFuncWorker = func(...any)
+
+type WorkerJob struct {
+	Cancel     bool
+	HandleFunc handleFuncWorker
+	Params     []any
+}
+
+type WorkerPool struct {
+	jobs       []WorkerJob
+	maxWorkers uint
+	wg         *sync.WaitGroup
+	mut        *sync.Mutex
+}
+
+func NewWorker(workerPool *WorkerPool, wg *sync.WaitGroup, id uint) {
+	go func() {
+		for {
+			if len(workerPool.jobs) > 0 {
+				workerPool.mut.Lock()
+				if len(workerPool.jobs) > 0 {
+					first := workerPool.jobs[0]
+					workerPool.jobs = workerPool.jobs[1:]
+					if first.Cancel {
+						wg.Done()
+						workerPool.mut.Unlock()
+						break
+					}
+					workerPool.mut.Unlock()
+					first.HandleFunc(first.Params...)
+					continue //This continue statement avoid the execute the second Unlock :!
+				}
+				workerPool.mut.Unlock()
+			}
+
+		}
+	}()
+}
+
+func NewWorkerPool(size uint) *WorkerPool {
+
+	mut := new(sync.Mutex)
+	wg := new(sync.WaitGroup)
+
+	workerPool := &WorkerPool{
+		wg:         wg,
+		mut:        mut,
+		jobs:       []WorkerJob{},
+		maxWorkers: size,
+	}
+
+	wg.Add(int(size))
+	for i := 0; i < int(size); i++ {
+		NewWorker(workerPool, wg, uint(i))
+	}
+	return workerPool
+}
+
+func (w *WorkerPool) Execute(f handleFuncWorker, p ...any) {
+	w.mut.Lock()
+	w.jobs = append(w.jobs, WorkerJob{
+		Cancel:     false,
+		HandleFunc: f,
+		Params:     p,
+	})
+	w.mut.Unlock()
+}
+
+func (w *WorkerPool) Wait() {
+	defer w.wg.Wait()
+	jobs := []WorkerJob{}
+	for i := 0; i < int(w.maxWorkers); i++ {
+		jobs = append(jobs, WorkerJob{
+			Cancel: true,
+			HandleFunc: func(p ...any) {
+			},
+			Params: []any{},
+		})
+	}
+	w.mut.Lock()
+	w.jobs = append(w.jobs, jobs...)
+	w.mut.Unlock()
+
+}
